2166cc9954ed68cd88655f131abcdcaddd6b86be
Lukas Henrique Dias <diaslukas19@gmail.com>
2022-05-20 11:59:57 -0300

    feat: firebase config

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,25 +1,7 @@
-import Avatar from 'components/Avatar'
-
 function App() {
   return (
-    <div className="bg-white">
-      <div className="py-16 px-4 mx-auto max-w-screen-xl sm:py-24 sm:px-6 lg:px-8">
-        <div className="text-center">
-          <h2 className="text-base font-semibold tracking-wide text-blue-600 uppercase">
-            Welcome to
-          </h2>
-          <p className="my-3 text-4xl font-bold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
-            reactjs-vite-tailwindcss-boilerplate
-          </p>
-          <p className="text-xl text-gray-400">Start building for free.</p>
-          <p className="mt-5">
-            <Avatar
-              size="large"
-              src="https://www.gravatar.com/avatar/4405735f6f3129e0286d9d43e7b460d0"
-            />
-          </p>
-        </div>
-      </div>
+    <div className="flex flex-col w-screen h-screen bg-black">
+      <h1 className="m-auto text-6xl font-bold text-white">My Firebase App</h1>
     </div>
   )
 }
